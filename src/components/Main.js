8a421786d19f463d469c285e11658726c8dfa49f
Orlando Leite Fernandes de Oliveira <orlandomv19@gmail.com>
2022-08-23 15:40:31 -0300

    aulas de react

---
+++ src/components/Main.js
@@ -0,0 +1,65 @@
+import React,{Component} from 'react';
+
+import {FaEdit, FaPlus,FaWindowClose} from 'react-icons/fa';
+import './main.css';
+
+export default class Main extends Component{
+    
+    state = {
+            novaTarefa: '',
+            tarefas: [],
+        };
+
+
+    handleChange = (e)  => {
+        this.setState({
+            novaTarefa:  e.target.value,
+        });
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        const {tarefas} = this.state;
+        let {novaTarefa} = this.state;
+        novaTarefa = novaTarefa.trim();
+
+        if(tarefas.indexOf(novaTarefa) !== -1) return;
+
+        const novasTarefas = [...tarefas];
+
+        this.setState({
+            tarefas: [...novasTarefas,novaTarefa],
+        });
+
+        console.log('Oi');
+    }
+_
+        render(){
+    const {novaTarefa,tarefas} = this.state;
+
+        return (
+            <div className = "main">
+                <h1>Lista de Tarefas</h1>
+
+                <form onSubmit={this.handleSubmit} action="#" className="form">
+                    <input type="text" onChange={this.handleChange} value={novaTarefa} />
+                    <button  type="submit">
+                        <FaPlus />
+                    </button>
+                </form>
+
+            <ul className="tarefas">
+                {tarefas.map((tarefa)=>(
+                    <li key= {tarefa}>
+                        {tarefa}
+                        <span>
+                            <FaEdit className="edit" />
+                            <FaWindowClose onClick={this.handleDelete} className ="delete" />
+                        </span>
+                    </li>
+                ))}
+            </ul>
+            </div>
+        );
+    }
+};
